74e1e9186415ac07ffa834dfd27c15a709879507
KunlinY <kunliny@qq.com>
2020-03-02 19:14:47 -0800

    finish custom

--- predictor.c
+++ predictor.c
@@ -162,8 +162,13 @@ void tournament_train(uint32_t pc, uint8_t outcome)
 
 	if (local != global)
 	{
-		uint8_t status = TAKEN;
+		uint8_t status = -1;
 		uint8_t prediction = tPredictor[globalhistory];
+
+		if (local == outcome)
+		{
+			status = TAKEN;
+		}
 
 		if (global == outcome)
 		{
